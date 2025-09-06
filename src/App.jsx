59ba923d6226fa994b33b911355f6a72ebc72829
mdasaduzzamanminhaz3 <mdasaduzzamanminhaz3@gmail.com>
2025-09-06 21:46:46 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Employee from './components/Employee'
+// import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Employee/>
+    </>
+  )
+}
+
+export default App

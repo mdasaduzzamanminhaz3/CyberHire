fd12d7b75fab11fa878571a5b4500f2950e8bd24
mdasaduzzamanminhaz3 <mdasaduzzamanminhaz3@gmail.com>
2025-09-07 19:28:43 +0600

    Force add experts.JSON for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import Employee from './components/Employee'
 // import './App.css'
 
